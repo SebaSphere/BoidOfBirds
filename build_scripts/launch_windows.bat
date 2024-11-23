825903bdfbbee23fece516749eef2868bed40e9a
SebaSphere <SebaSphereEmails@gmail.com>
2024-11-23 02:27:56 -0500

    feat: now we actually bundle the JRE

---
+++ build_scripts/launch_windows.bat
@@ -0,0 +1,4 @@
+@echo off
+out-windows64\BoidOfBirds.exe
+pause
+
---
+++ build_scripts/packer-windows.bat
@@ -0,0 +1,3 @@
+java -jar packr-all-4.0.0.jar --platform windows64 --jdk zulu21.38.21-ca-jre21.0.5-win_x64.zip --useZgcIfSupportedOs --executable BoidOfBirds --classpath BoidOfBirds-1.0.2-SNAPSHOT.jar --mainclass dev.sebastianb.boids.Main --resources ../src/main/resources --output out-windows64
+
+PAUSE
